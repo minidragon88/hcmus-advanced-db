586d9bb9c9d044c0011c07e5b5058d5736b2f470
Phu Nguyen <phu@treasure-data.com>
2020-04-06 14:43:41 +0700

    Init gradle task to run main class

---
+++ log-generator/src/main/java/hcmus/advanced_db/log_generator/Generator.java
@@ -0,0 +1,10 @@
+package hcmus.advanced_db.log_generator;
+
+public class Generator {
+
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("abc");
+    }
+
+}
